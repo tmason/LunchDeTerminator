50b757af5f3b94a44ac132f5bf29991bc9c9cf9e
Tyler Mason <tmason@360incentives.com>
2015-10-27 10:20:05 -0400

    Push Again
    Moar stuff

--- ArnoldCHacks/RandomNode.scala
+++ ArnoldCHacks/RandomNode.scala
@@ -1,5 +1,15 @@
 package org.arnoldc.ast
 
-import org.objectweb.asm.MethodVisitor
+import org.objectweb.asm.{Opcodes, MethodVisitor, Label}
 import org.objectweb.asm.Opcodes._
 import org.arnoldc.SymbolTable
+import scala.util.Random
+
+case class RandomNode(expression: AstNode) extends NumberNode {
+	def generate(mv: MethodVisitor, SymbolTable: SymbolTable) {
+		var RandomNum = new scala.util.Random
+		1 to 1000 map{ _ => RandomNum.nextInt(1)}
+
+		mv.visitIntInsn(SIPUSH, RandomNum)
+	}
+}
