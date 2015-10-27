c8304c78baa1c80d1441c48290526f274b9a6bd2
unknown <RAhmed@LAPTOP-0002.360Incentives.local>
2015-10-27 10:33:53 -0400

    Added obnoxious sound effects to ALL links

--- scripts.js
+++ scripts.js
@@ -2,10 +2,3 @@ function playSound(soundfile) {
 	document.getElementById("dummy").innerHTML=
 	"<embed src=\""+soundfile+"\" hidden=\"true\" autostart=\"true\" loop=\"false\" />";
 }
-
-function playRandomFile() {
-	var sourceFolder = Folder("C:\\Lunch De-Terminator\\LunchDeTerminator\\RandomArnoldQuotes");
-	var fileList = sourceFolder.getFiles();
-	
-	playSound(fileList[0].Name);
-}
