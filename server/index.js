acb755bf52cf647328d2fe485a23d6f2913e85c1
m-white <mwhite@360incentives.com>
2015-10-27 13:05:10 -0400

    get data from arnoldc

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ var http = require('http');
 var app = express();
 var server = http.Server(app);
 var router = express.Router();
+var io = require('socket.io')(server);
+
+var getFood = require('../lib/get-food.js');
 
 var HomeRoute = require('./routes/home');
 
@@ -11,6 +14,14 @@ module.exports = function createServer(config, cb) {
   app.use(express.static('styles'));
   app.use(express.static('assets'));
   app.use(express.static('lib'));
+
+  io.on('connection', function (socket) {
+    socket.on('getFood', function () {
+      var result = getFood();
+
+      socket.emit('returnFood',  { data: result });
+    });
+  });
 
   app.get('/', HomeRoute);
 
