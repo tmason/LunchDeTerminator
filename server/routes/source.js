bcf1cbf66092786bfb987b5eb2c8d63dce72c4c6
m-white <mwhite@360incentives.com>
2015-11-01 12:10:20 -0500

    initial commit of new source page

--- server/routes/source.js
+++ server/routes/source.js
@@ -1,11 +1,20 @@
 'use strict';
 
+var fs = require('fs');
 var React = require('react');
 
 var SourceView = React.createFactory(require('../../web/views/source.jsx'));
 
 module.exports = function SourceRoute(req, res) {
+  var reactProps, sourceCode;
+
+  sourceCode = fs.readFileSync('lib/determinator.arnoldc', 'utf-8');
+  reactProps = {
+    sourceCode: sourceCode
+  };
+
   res.status(200).render('__layout', {
-    outlet: React.renderToString(new SourceView(req.reactProps))
+    outlet: React.renderToString(new SourceView(reactProps)),
+    props: JSON.stringify(reactProps)
   });
 };
--- web/views/source.jsx
+++ web/views/source.jsx
@@ -1,12 +1,27 @@
 'use strict';
 
 var React = require('react');
+var Button = require('react-bootstrap/lib/Button');
 
 var SourceView = React.createClass({
 
   render: function () {
+    var sourceCode = this.props.sourceCode.split(/\n/);
+    var codeList = sourceCode.map(function (line) {
+      return <p> { line } </p>;
+    });
+
     return (
       <div className='source'>
+        <Button className='btn-home'
+        href='/'
+        bsSize='large'>
+          {'Back To Home'}
+        </Button>
+
+        <div className='code'>
+          { codeList }
+        </div>
       </div>
     );
   }
